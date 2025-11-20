e8bf7ea748526ba622023eecb016a644a8546cd6
Hung hay ho <hungok2909@gmail.com>
2025-11-20 21:27:49 +0700

    RC5: chore: Added role + permission repository

---
+++ src/main/java/iuh/fit/se/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package iuh.fit.se.repositories;
+
+import iuh.fit.se.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
